ee7829b54ef584e4f0be0d508bc2c69b2e6883ff
Sven Marcus <s.marcus@outlook.de>
2018-11-28 20:34:42 +0100

    FormBuilder prototype

---
+++ builderTest.py
@@ -0,0 +1,18 @@
+from PyQt5.QtWidgets import QApplication
+
+from ui.qtformbuilder import FormBuilder
+
+app = QApplication([])
+
+builder = FormBuilder()
+
+builder.form()
+builder.add_input("Test1", "str")
+builder.add_input("Test2", "str")
+widget = builder.finish()
+
+widget.onConfirm.connect(lambda: print(widget.getContent("Test1")))
+widget.onCancel.connect(lambda: print(widget.getContent("Test2")))
+widget.show()
+
+app.exec()
---
+++ ui/qtformbuilder.py
@@ -0,0 +1,88 @@
+from abc import ABC, abstractmethod
+from typing import Any
+from PyQt5.QtWidgets import QGridLayout, QWidget, QLineEdit, QPushButton, QLabel, QHBoxLayout
+from PyQt5.QtCore import pyqtSignal
+
+class FormWidget(ABC):
+
+    @abstractmethod
+    def getContent(self) -> Any:
+        pass
+
+    @abstractmethod
+    def getWidget(self) -> QWidget:
+        pass
+
+
+class Form(QWidget):
+
+    onConfirm: pyqtSignal = pyqtSignal()
+    onCancel: pyqtSignal = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.__inputFields = {}
+        self.__ok = QPushButton("Ok")
+        self.__cancel = QPushButton("Cancel")
+
+        self.__ok.clicked.connect(self.__onConfirm)
+        self.__cancel.clicked.connect(self.__onCancel)
+
+        self.setLayout(QGridLayout())
+        self.__rows = 0
+
+
+    def __onConfirm(self) -> None:
+        self.onConfirm.emit()
+
+    def __onCancel(self) -> None:
+        self.onCancel.emit()
+
+    def show(self):
+        buttonBar = QWidget()
+        buttonBar.setLayout(QHBoxLayout())
+        buttonBar.layout().addWidget(self.__ok)
+        buttonBar.layout().addWidget(self.__cancel)
+        self.layout().addWidget(buttonBar, self.__rows, 0, 1, -1)
+        super().show()
+
+    def addWidget(self, label: str, formWidget: FormWidget):
+        self.__inputFields[label] = formWidget
+        self.layout().addWidget(QLabel(label), self.__rows, 0)
+        self.layout().addWidget(formWidget.getWidget(), self.__rows, 1)
+        self.__rows += 1
+
+    def getContent(self, label: str) -> Any:
+        return self.__inputFields[label].getContent()
+
+
+class TextWidget(FormWidget):
+
+    def __init__(self):
+        self.__widget: QLineEdit = QLineEdit()
+    
+    def getContent(self) -> str:
+        return self.__widget.text()
+
+    def getWidget(self) -> QWidget:
+        return self.__widget
+
+
+class FormBuilder:
+
+    def __init__(self):
+        self.__form = None
+
+    def form(self):
+        self.__form: QWidget = Form()
+
+        return self
+
+    def add_input(self, label: str, input_type: str):
+        if input_type == "str":
+            self.__form.addWidget(label, TextWidget())
+        
+        return self
+
+    def finish(self) -> Form:
+        return self.__form
