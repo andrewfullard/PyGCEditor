cb07d3c846c07db0b06eb2817e745a42201a0be9
Corey <ThrawnsRevenge@gmail.com>
2022-01-15 23:36:51 -0500

    Import forces button

--- ui/qtmainwindow.py
+++ ui/qtmainwindow.py
@@ -97,6 +97,11 @@ class QtMainWindow(MainWindow):
         self.__totalFactionForceLabel: QLabel = QLabel()
         self.__totalFactionForceLabel.setText("Select a campaign to see total starting forces per faction")
 
+        self.__importStartingForcesButton: QPushButton = QPushButton("Import Default Forces")
+        self.__importStartingForcesButton.clicked.connect(
+            self.__importStartingForcesButtonClicked
+        )
+
         # set up menu and menu options
         self.__menuBar: QMenuBar = QMenuBar()
         self.__fileMenu: QMenu = QMenu("File", self.__window)
@@ -162,6 +167,8 @@ class QtMainWindow(MainWindow):
         self.__startingForces.layout().addWidget(self.__planetComboBox)
         self.__startingForces.layout().addWidget(self.__forcesListTable)
         self.__startingForces.layout().addWidget(self.__planetInfoLabel)
+        self.__startingForces.layout().addWidget(self.__importStartingForcesButton)
+
         # self.__startingForces.layout().addWidget(self.__totalPlanetForceLabel)
         # self.__startingForces.layout().addWidget(self.__totalFactionForceLabel)
 
@@ -381,6 +388,8 @@ class QtMainWindow(MainWindow):
             self.__uncheckAllTable(table)
 
             self.__presenter.allPlanetsChecked(False)
+    def __importStartingForcesButtonClicked(self) -> None:
+        """Imports all starting forces from spreadsheets"""
 
     def __selectAllTradeRoutesButtonClicked(
         self, table: QTableWidget, checked: bool
